2123e2d22e17d0a9ad5d83016b5e0b4bc1b418ce
AlBattalSara <YOUR GITHUB EMAIL>
2019-12-31 19:24:41 +0300

    Hw API memes

---
+++ app.js
@@ -0,0 +1,23 @@
+// Show the Image and their names including the api width and height
+
+
+const URL_MEME = "https://api.imgflip.com/get_memes"; 
+
+let response = {}
+function memes(){
+    axios.get(URL_MEME).then(function(show){
+    
+        response= show.data;
+        // response= response.data.memes;
+        console.log(response);
+        response.data.memes.forEach(element => { 
+            
+            document.getElementById("ID").innerHTML +=` <div>${element.name}</div> <img src= ${element.url} width=${element.width} height=${element.height} >`
+            
+        });
+     
+        
+        
+    });
+
+}
